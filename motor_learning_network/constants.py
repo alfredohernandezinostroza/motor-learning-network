02811cf96123d3d5fe8c9f246d5e89913d8a8b92
Alfredo <alfredo.hernandez@ug.uchile.cl>
2026-02-04 17:04:47 +0000

    feat(constants): add MY_EMAIL to constants

--- motor_learning_network/constants.py
+++ motor_learning_network/constants.py
@@ -1,4 +1,9 @@
 from pathlib import Path 
+import dotenv
+import os
 
 DATA_PATH = Path("data","raw")
 DATA_PATH.mkdir(exist_ok=True)
+
+dotenv.load_dotenv(Path('..','.env'))
+MY_EMAIL = os.getenv('MY_EMAIL')
