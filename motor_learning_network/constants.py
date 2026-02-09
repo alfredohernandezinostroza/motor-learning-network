2bf9c2bdaf6defb982793db2fb896e5268f2134b
Alfredo <alfredo.hernandez@ug.uchile.cl>
2026-02-09 14:39:32 +0000

    feat(pipelines): add processed data and figures paths to constants

--- motor_learning_network/constants.py
+++ motor_learning_network/constants.py
@@ -2,8 +2,14 @@ from pathlib import Path
 import dotenv
 import os
 
-DATA_PATH = Path("data","raw")
-DATA_PATH.mkdir(exist_ok=True)
+RAW_DATA_PATH = Path("data","raw")
+RAW_DATA_PATH.mkdir(exist_ok=True)
+
+PROCESSED_DATA_PATH = Path("data","processed")
+PROCESSED_DATA_PATH.mkdir(exist_ok=True)
+
+FIGURES_PATH = Path("reports","figures")
+FIGURES_PATH.mkdir(exist_ok=True)
 
 dotenv.load_dotenv(Path('..','.env'))
-MY_EMAIL = os.getenv('MY_EMAIL')
+EMAIL = os.getenv('MY_EMAIL')
