d8adeab08eb13192b278c6c9af25b9ef1ab4e386
Alfredo <alfredo.hernandez@ug.uchile.cl>
2026-03-06 12:51:59 +0000

    fix(cosntants): add assertions and fix env variables loading

--- motor_learning_network/constants.py
+++ motor_learning_network/constants.py
@@ -11,9 +11,16 @@ PROCESSED_DATA_PATH.mkdir(exist_ok=True)
 FIGURES_PATH = Path("reports","figures")
 FIGURES_PATH.mkdir(exist_ok=True)
 
-dotenv.load_dotenv(Path('..','.env'))
-EMAIL = os.getenv('MY_EMAIL')
 DEFAULT_UI_PROJECT_ID = 1
+
+read_dotenv = dotenv.load_dotenv(Path(__file__).parent.parent / '.env')
+assert read_dotenv, f"Failed to read .env file ast {Path(__file__).parent.parent / '.env'}"
+assert os.getenv("MY_EMAIL") is not None, "MY_EMAIL not found in .env file"
+assert os.getenv("DEFAULT_UI_USERNAME") is not None, "DEFAULT_UI_USERNAME not found in .env file"
+assert os.getenv("TEAM_NAME") is not None, "TEAM_NAME not found in .env file"
+assert os.getenv("GOOGLE_DRIVE_FOLDER_ID") is not None, "GOOGLE_DRIVE_FOLDER_ID not found in .env file"
+
+EMAIL = os.getenv('MY_EMAIL')
 DEFAULT_UI_USERNAME = os.getenv('DEFAULT_UI_USERNAME')
 TEAM_NAME = os.getenv('TEAM_NAME')
-
+GOOGLE_DRIVE_FOLDER_ID = os.getenv('GOOGLE_DRIVE_FOLDER_ID')
