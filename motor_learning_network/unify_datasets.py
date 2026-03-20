d01502834e3dc00ceb8d1bcd79fee2eb09d67f1c
Alfredo <alfredo.hernandez@ug.uchile.cl>
2026-03-20 16:15:39 +0000

    feat(pieplines): upgrade cleaning for scopus and pubmed, update schema

--- motor_learning_network/unify_datasets.py
+++ motor_learning_network/unify_datasets.py
@@ -14,7 +14,7 @@ from motor_learning_network.constants import (
 )
 from pathlib import Path
 from hamilton.io.materialization import from_, to
-
+from typing import List
 ###################
 ##   Constants   ##
 ###################
@@ -24,7 +24,6 @@ UI_CONFIG = adapters.HamiltonTracker(
     username=DEFAULT_UI_USERNAME,
     dag_name=CURRENT_FILE_NAME,
     tags={"environment": "DEV", "team": TEAM_NAME, "version": "0.1"},
-    
 )
 
 
@@ -44,9 +43,9 @@ def _main() -> int:
     unified_database_schema = pa.DataFrameSchema(
         dict(
             title=pa.Column(str),
-            authors=pa.Column(str),
+            authors=pa.Column(List[str]),
             abstract=pa.Column(str),
-            keywords=pa.Column(str),
+            keywords=pa.Column(List[str]),
             journal=pa.Column(str),
             source_database=pa.Column(str),
             doi=pa.Column(
@@ -71,6 +70,11 @@ def _main() -> int:
             id="merged_dataframes__parquet",
             dependencies=["merged_dataframes"],
             path=PROCESSED_DATA_PATH / "unified_database.parquet",
+        ),
+        to.parquet(
+            id="scopus_author_ids__parquet",
+            dependencies=["scopus_author_ids"],
+            path=PROCESSED_DATA_PATH / "scopus_author_ids.parquet",
         ),
     ]
     #######################
@@ -105,26 +109,50 @@ def _main() -> int:
     return 0
 
 
+def scopus_author_ids(scopus_df: pd.DataFrame) -> pd.DataFrame:
+    """Saves author ids to a dataframe. Some of them will be missing because there rows without the same number of
+    ids vs number of rows (this is a problem from Scopus, the missing authors can be added manually, but it would take some time"""
+    scopus_author_ids = scopus_df["Author(s) ID"].str.split(
+        "; "
+    )  # extract the IDs, they could be useful
+    author_names_with_ids = pd.concat([scopus_df["authors"], scopus_author_ids], axis=1)
+    author_names_with_ids.columns = ["authors", "author_ids"]
+    author_names_with_ids = author_names_with_ids[
+        author_names_with_ids["authors"].apply(len)
+        == author_names_with_ids["author_ids"].apply(len)
+    ]  # Filter out rows where list lengths do not match
+    author_names_with_ids = author_names_with_ids.explode(
+        ["authors", "author_ids"]
+    )  # pair each author with corresponding ID
+    author_names_with_ids["author_ids"] = pd.to_numeric(
+        author_names_with_ids["author_ids"]
+    ).astype("Int64")
+    return author_names_with_ids
+
+
 def scopus_df_clean(
     scopus_df: pd.DataFrame, unified_database_schema: pa.DataFrameSchema
 ) -> pd.DataFrame:
     renaming_columns_map = {
         "Title": "title",
-        "Authors": "authors",
+        "Author full names": "authors",
         "Abstract": "abstract",
         "Author Keywords": "keywords",
         "Source title": "journal",
         "Source": "source_database",
         "DOI": "doi",
         "PubMed ID": "pubmed_id",
-        "Year": "year"
+        "Year": "year",
     }
     scopus_df = scopus_df.rename(columns=renaming_columns_map)
-    scopus_df = scopus_df[list(unified_database_schema.columns.keys())]
+    scopus_df = scopus_df[list(unified_database_schema.columns.keys())]  #keep only the columns defined in the schema
     scopus_df["doi"] = scopus_df["doi"].str.lower()
-    scopus_df = scopus_df.replace("nan","")
+    scopus_df = scopus_df.replace("nan", "")
+    scopus_df["keywords"] = scopus_df["keywords"].str.split('; ')
     scopus_df["pubmed_id"] = pd.to_numeric(scopus_df["pubmed_id"]).astype("Int64")
     scopus_df["year"] = pd.to_numeric(scopus_df["year"]).astype("Int64")
+    scopus_df["authors"] = scopus_df["authors"].str.replace(pat=" \(.*?\)", repl="", regex=True)
+    scopus_df["authors"] = scopus_df["authors"].str.split("; ")
     unified_database_schema.validate(scopus_df)
     return scopus_df
 
@@ -141,16 +169,18 @@ def wos_df_clean(
         # "": "source_database", #no source_database column here, it will be added later
         "DI": "doi",
         "PM": "pubmed_id",
-        "PY": "year"
+        "PY": "year",
     }
     wos_df = wos_df.rename(columns=renaming_columns_map)
     wos_df["source_database"] = "Web of Science"
-    wos_df = wos_df[list(unified_database_schema.columns.keys())]
+    wos_df = wos_df[list(unified_database_schema.columns.keys())]  #keep only the columns defined in the schema
     wos_df["doi"] = wos_df["doi"].str.lower()
-    wos_df = wos_df.replace("nan","")
+    wos_df = wos_df.replace("nan", "")
     wos_df["pubmed_id"] = pd.to_numeric(wos_df["pubmed_id"]).astype("Int64")
-    # wos_df["year"] = wos_df["year"].str[-4:] # the date format of WoS is of the type: NOV 2 2019
     wos_df["year"] = pd.to_numeric(wos_df["year"]).astype("Int64")
+    wos_df["keywords"] = wos_df["keywords"].str.replace(pat="<(.*?)>", repl="", regex=True)#some keywords had weird HTML tags on them
+    wos_df["keywords"] = wos_df["keywords"].str.split('; ')
+    wos_df["authors"] = wos_df["authors"].str.split('; ')
     unified_database_schema.validate(wos_df)
     return wos_df
 
@@ -158,19 +188,39 @@ def wos_df_clean(
 def pubmed_df_clean(
     pubmed_df: pd.DataFrame, unified_database_schema: pa.DataFrameSchema
 ) -> pd.DataFrame:
-    renaming_columns_map = { #most have the same names, I left them (commented out) just for reference
+    def _get_authors_from_series(papers: pd.Series) -> str:
+        papers_with_parsed_authors = []
+        for authors in papers:
+            authors_list = []
+            for author in authors:
+                if author["firstname"] is None:
+                    author["firstname"] = ""
+                if author["lastname"] is None:
+                    author["lastname"] = ""
+                new_author = (
+                    f"{author['firstname']} {author['lastname']}"
+                )
+                if new_author == " ":
+                    continue
+                authors_list.append(new_author)
+            papers_with_parsed_authors.append(authors_list)
+        return pd.Series(papers_with_parsed_authors)
+    renaming_columns_map = {  # most have the same names, I left them (commented out) just for reference
         # "title": "title",
-        "full_author": "authors",
+        # "authors": "authors",
         # "abstract": "abstract",
-        "author_keywords": "keywords",
+        # "keywords": "keywords",
         # "journal": "journal",
-        "source": "source_database",
+        # "": "source_database", #this one is not presenmt, it will be added manually later
         # "doi": "doi",
         # "pubmed_id": "pubmed_id",
-        # "year": "year"
+        "publication_date": "year"
     }
     pubmed_df = pubmed_df.rename(columns=renaming_columns_map)
-    pubmed_df = pubmed_df[list(unified_database_schema.columns.keys())]
+    pubmed_df["source_database"] = "PubMed"
+    pubmed_df = pubmed_df[list(unified_database_schema.columns.keys())]  #keep only the columns defined in the schema
+    pubmed_df["authors"] = _get_authors_from_series(pubmed_df["authors"])
+    pubmed_df["pubmed_id"] = pd.to_numeric(pubmed_df["pubmed_id"]).astype("Int64")
     unified_database_schema.validate(pubmed_df)
     return pubmed_df
 
@@ -178,9 +228,28 @@ def pubmed_df_clean(
 def ebsco_df_clean(
     ebsco_df: pd.DataFrame, unified_database_schema: pa.DataFrameSchema
 ) -> pd.DataFrame:
-    ebsco_df = ebsco_df[unified_database_schema.columns]
+    
+    renaming_columns_map = {
+        # "title": "title",
+        "contributors": "authors",
+        # "abstract": "abstract",
+        "subject": "keywords",
+        "source": "journal",
+        "longDBName": "source_database",
+        # "doi": "doi",
+        # "": "pubmed_id", it does not have it, we will have to add an empty column
+        "publicationDate": "year",
+    }
+    ebsco_df = ebsco_df.rename(columns=renaming_columns_map)
+    ebsco_df = ebsco_df[list(unified_database_schema.columns.keys())]  #keep only the columns defined in the schema
+    ebsco_df["doi"] = ebsco_df["doi"].str.lower()
+    ebsco_df = ebsco_df.replace("nan", "")
+    ebsco_df["keywords"] = ebsco_df["keywords"].str.split('; ')
+    ebsco_df["pubmed_id"] = pd.to_numeric(ebsco_df["pubmed_id"]).astype("Int64")
+    ebsco_df["year"] = pd.to_numeric(ebsco_df["year"]).astype("Int64")
+    ebsco_df["authors"] = ebsco_df["authors"].str.replace(pat=" \(.*?\)", repl="", regex=True)
+    ebsco_df["authors"] = ebsco_df["authors"].str.split("; ")
     unified_database_schema.validate(ebsco_df)
-    return ebsco_df
 
 
 def merged_dataframes(
